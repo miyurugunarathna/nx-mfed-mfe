a9a7448fe55c180965581ea50645912da5c4edfb
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2025-04-12 23:41:20 +0530

    feat: initiated ui library

--- apps/host/tailwind.config.js
+++ apps/host/tailwind.config.js
@@ -1,4 +1,4 @@
-// const { createGlobPatternsForDependencies } = require('@nx/next/tailwind');
+const { createGlobPatternsForDependencies } = require('@nx/next/tailwind');
 
 // The above utility import will not work if you are using Next.js' --turbo.
 // Instead you will have to manually add the dependent paths to be included.
@@ -14,10 +14,65 @@ module.exports = {
   content: [
     './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
     '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
-    //     ...createGlobPatternsForDependencies(__dirname)
+    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        card: {
+          DEFAULT: 'var(--card)',
+          foreground: 'var(--card-foreground)',
+        },
+        popover: {
+          DEFAULT: 'var(--popover)',
+          foreground: 'var(--popover-foreground)',
+        },
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        'chart-1': 'var(--chart-1)',
+        'chart-2': 'var(--chart-2)',
+        'chart-3': 'var(--chart-3)',
+        'chart-4': 'var(--chart-4)',
+        'chart-5': 'var(--chart-5)',
+        sidebar: {
+          DEFAULT: 'var(--sidebar)',
+          foreground: 'var(--sidebar-foreground)',
+        },
+        'sidebar-primary': {
+          DEFAULT: 'var(--sidebar-primary)',
+          foreground: 'var(--sidebar-primary-foreground)',
+        },
+        'sidebar-accent': {
+          DEFAULT: 'var(--sidebar-accent)',
+          foreground: 'var(--sidebar-accent-foreground)',
+        },
+        'sidebar-border': 'var(--sidebar-border)',
+        'sidebar-ring': 'var(--sidebar-ring)',
+      },
+    },
   },
   plugins: [],
 };
---
+++ packages/ui/README.md
@@ -0,0 +1,7 @@
+# @nx-mfed-mfe/ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @nx-mfed-mfe/ui` to execute the unit tests via [Jest](https://jestjs.io).
